02d601c883c21e02d3e8f243c3e4c13b2e4495dd
Durgesh Rao <77789927+DURGESH716@users.noreply.github.com>
2022-03-04 11:21:18 +0530

    Update Binary_Search.py

--- Binary_Search.py
+++ Binary_Search.py
@@ -1,18 +1,20 @@
 def binarySearch(array, x, low, high):
-  
+    
     # Repeat until the pointers low and high meet each other
     while low <= high:
         mid = low + (high - low)//2
+        
         if array[mid] == x:
             return mid
+          
         elif array[mid] < x:
             low = mid + 1
+            
         else:
             high = mid - 1
+            
     return -1
 
-
-  
 array = [3, 4, 5, 6, 7, 8, 9]
 x = 4
 

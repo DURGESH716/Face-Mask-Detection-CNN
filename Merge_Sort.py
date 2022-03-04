003696d5c99487063d03bad80f2f7f03464ba5e0
Durgesh Rao <77789927+DURGESH716@users.noreply.github.com>
2022-03-04 11:23:18 +0530

    Update Merge_Sort.py

--- Merge_Sort.py
+++ Merge_Sort.py
@@ -1,6 +1,7 @@
 def mergeSort(array):
-  
     if len(array) > 1:
+
+        #  r is the point where the array is divided into two subarrays
         r = len(array)//2
         L = array[:r]
         M = array[r:]
@@ -34,12 +35,19 @@ def mergeSort(array):
             j += 1
             k += 1
 
+
+# Print the array
 def printList(array):
     for i in range(len(array)):
         print(array[i], end=" ")
     print()
-    
-array = [6, 5, 12, 10, 9, 1]
-mergeSort(array)
-print("Sorted array is: ")
-printList(array)
+
+
+# Driver program
+if __name__ == '__main__':
+    array = [6, 5, 12, 10, 9, 1]
+
+    mergeSort(array)
+
+    print("Sorted array is: ")
+    printList(array)
